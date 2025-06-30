db4f5a1966fdbee337f29c3eeffd4ab38d460755
juni <transrights@ju.niper.me>
2025-06-30 04:24:46 +0200

    added feature for clog, partial level, chat messages, a lot tbh

--- src/main/java/com/pyonium/clicker/ClickerConfig.java
+++ src/main/java/com/pyonium/clicker/ClickerConfig.java
@@ -1,9 +1,88 @@
 package com.pyonium.clicker;
 
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.*;
 
 @ConfigGroup("clicker")
 public interface ClickerConfig extends Config
 {
+    @ConfigItem(
+            keyName = "clickerVolume",
+            name = "Volume",
+            description = "How loud does puppy want its reward?",
+            position = 10
+    )
+    default int volume()
+    {
+        return 100;
+    }
+
+    @ConfigItem(
+            keyName = "chatMessages",
+            name = "Chat Messages",
+            description = "Do you want chat messages?",
+            position = 20
+    )
+    default boolean chatMessages()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "onLevel",
+            name = "Click On Level Up",
+            description = "Rewards level ups",
+            position=30
+    )
+    default boolean onLevel()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "onVirtualLevel",
+            name = "Click On Virtual Level",
+            description = "For all the maxed puppies out there!",
+            position=35
+    )
+    default boolean onVirtualLevel()
+    {
+        return true;
+    }
+
+    @ConfigItem(
+            keyName = "onPartLevel",
+            name = "Click On Partial Level",
+            description = "For if it takes too long!",
+            position=40
+    )
+    default boolean onPartLevel()
+    {
+        return false;
+    }
+
+    @ConfigItem(
+            keyName = "levelPartSize",
+            name = "Clicks Per Level",
+            description = "How many times do you want to be rewarded per level?",
+            position=41
+    )
+    default int levelPartSize()
+    {
+        return 2;
+    }
+
+    @ConfigItem(
+            keyName = "onClog",
+            name = "Click On Collection Log",
+            description = "Rewards Collection Log Slots",
+            position=50
+    )
+    default boolean onClog()
+    {
+        return true;
+    }
+
+
+
+
 }
--- src/main/java/com/pyonium/clicker/ClickerPlugin.java
+++ src/main/java/com/pyonium/clicker/ClickerPlugin.java
@@ -1,11 +1,19 @@
 package com.pyonium.clicker;
 
 import javax.inject.Inject;
+
+import com.google.inject.Provides;
 import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
-import net.runelite.api.events.GameStateChanged;
+import net.runelite.api.events.ChatMessage;
 import net.runelite.api.events.StatChanged;
+import net.runelite.client.chat.ChatColorType;
+import net.runelite.client.chat.ChatMessageBuilder;
+import net.runelite.client.chat.ChatMessageManager;
+import net.runelite.client.chat.QueuedMessage;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 
@@ -16,19 +24,26 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import net.runelite.client.RuneLite;
+import net.runelite.client.util.Text;
 
-import java.util.Arrays;
 import java.util.EnumMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Clicker Training"
+		name = "Clicker Training"
 )
 public class ClickerPlugin extends Plugin
 {
 	@Inject
 	private Client client;
+
+	@Inject
+	private ClickerConfig config;
+
+	@Inject
+	private ChatMessageManager chatMessageManager;
 
 	private final Map<Skill, Integer> oldExperience = new EnumMap<>(Skill.class);
 
@@ -39,12 +54,27 @@ public class ClickerPlugin extends Plugin
 			CLICKER_SOUND_FILE
 	};
 
+	private static final Pattern COLLECTION_LOG_ITEM_REGEX = Pattern.compile("New item added to your collection log:.*");
+
 	private Clip clip = null;
+
+	private boolean onLevel;
+	private boolean onPartLevel;
+	private boolean onClog;
+	private int volume;
+	private boolean onVirtualLevel;
+	private int levelPartSize;
+	private boolean chatMessages;
 
 	@Override
 	protected void startUp()
 	{
 		initSoundFiles();
+		this.onLevel = config.onLevel();
+		this.onClog = config.onClog();
+		this.volume = config.volume();
+		this.onPartLevel = config.onPartLevel();
+
 	}
 
 	@Override
@@ -54,11 +84,11 @@ public class ClickerPlugin extends Plugin
 		clip = null;
 	}
 
-	//@Inject
-	//private BloodShardNotifierConfig config;
-	
 	@Subscribe
 	public void onStatChanged(StatChanged statChanged) {
+		if (!onLevel) {
+			return;
+		}
 		final Skill skill = statChanged.getSkill();
 
 		// Modified from Nightfirecat's virtual level ups plugin as this info isn't (yet?) built in to statChanged event
@@ -69,16 +99,64 @@ public class ClickerPlugin extends Plugin
 
 		oldExperience.put(skill, xpAfter);
 
-		// Do not proceed if any of the following are true:
+		boolean fire = true;
+
+		//fire when threshold is between old xp and new xp (this xp drop passed it or reached it)
+
+		// Do not proceed if any of the following are true (sanity checks):
 		//  * xpBefore == -1              (don't fire when first setting new known value)
 		//  * xpAfter <= xpBefore         (do not allow 200m -> 200m exp drops)
-		//  * levelBefore >= levelAfter   (stop if if we're not actually reaching a new level)
 		//  * levelAfter > MAX_REAL_LEVEL && config says don't include virtual (level is virtual and config ignores virtual)
-		if (xpBefore == -1 || xpAfter <= xpBefore || levelBefore >= levelAfter ||
-				(levelAfter > Experience.MAX_REAL_LEVEL)) {
+		if (xpBefore == -1 || xpAfter <= xpBefore || (levelAfter > Experience.MAX_REAL_LEVEL && !onVirtualLevel)) {
 			return;
 		}
-		playSound(CLICKER_SOUND_FILE);
+
+		//fire if half level is reached
+		if(onPartLevel)
+		{
+			for(int i = 0; i < levelPartSize - 1; i++) {
+
+				int partway = Experience.getXpForLevel(levelBefore) + (Experience.getXpForLevel(levelBefore + 1) - Experience.getXpForLevel(levelBefore)) / levelPartSize * (i+1);
+
+				if (xpBefore < partway && xpAfter >= partway) {
+					String message = "You're " + (i + 1) + "/" + levelPartSize + " of the way to " + statChanged.getSkill().getName() + " level " + (levelBefore + 1) + "! Good puppy!";
+					sendHighlightedMessage(message);
+					playSound(CLICKER_SOUND_FILE);
+					return;
+				}
+			}
+		}
+
+		// * levelBefore >= levelAfter (new level is reached)
+		if(levelBefore < levelAfter)
+		{
+			playSound(CLICKER_SOUND_FILE);
+			return;
+		}
+
+
+
+	}
+
+	@Subscribe
+	public void onChatMessage(ChatMessage chatMessage)
+	{
+		if(!onClog)
+		{
+			return;
+		}
+		ChatMessageType msgType = chatMessage.getType();
+		if(!msgType.equals(ChatMessageType.GAMEMESSAGE))
+		{
+			return;
+		}
+
+		String outputMessage = Text.removeTags(chatMessage.getMessage());
+		if(COLLECTION_LOG_ITEM_REGEX.matcher(outputMessage).matches())
+		{
+			sendHighlightedMessage("A new item in your collection log! Good puppy!");
+			playSound(CLICKER_SOUND_FILE);
+		}
 	}
 
 
@@ -130,7 +208,6 @@ public class ClickerPlugin extends Plugin
 			clip.open(is);
 
 			//set volume
-			int volume = 100;
 			float vol = volume/100.0f;
 			FloatControl gainControl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
 			gainControl.setValue(20.0f * (float) Math.log10(vol));
@@ -142,5 +219,39 @@ public class ClickerPlugin extends Plugin
 			log.warn(f.getName());
 			log.warn("Sound file error", e);
 		}
+	}
+
+	@Provides
+	ClickerConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(ClickerConfig.class);
+	}
+
+	@Subscribe
+	public void onConfigChanged(ConfigChanged event)
+	{
+		this.onLevel = config.onLevel();
+		this.onClog = config.onClog();
+		this.volume = config.volume() > 100 ? 100 : config.volume();
+		this.onPartLevel = config.onPartLevel();
+		this.levelPartSize = config.levelPartSize();
+		this.onVirtualLevel = config.onVirtualLevel();
+		this.chatMessages = config.chatMessages();
+	}
+
+	private void sendHighlightedMessage(String message) {
+		if(!chatMessages)
+		{
+			return;
+		}
+		String highlightedMessage = new ChatMessageBuilder()
+				.append(ChatColorType.HIGHLIGHT)
+				.append(message)
+				.build();
+
+		chatMessageManager.queue(QueuedMessage.builder()
+				.type(ChatMessageType.CONSOLE)
+				.runeLiteFormattedMessage(highlightedMessage)
+				.build());
 	}
 }
